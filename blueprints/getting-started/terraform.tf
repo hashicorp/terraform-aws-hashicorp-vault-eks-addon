071fedb7102fc5fde856fb0c6a9fd98fe44adaec
Kerim Satirli <k.satirli@gmail.com>
2022-04-12 15:21:09 +0200

    bumps provider versions

--- blueprints/getting-started/terraform.tf
+++ blueprints/getting-started/terraform.tf
@@ -2,19 +2,22 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
+    # See https://registry.terraform.io/providers/hashicorp/aws/3.75.1
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.66.0"
+      version = ">= 3.75.1"
     }
 
+    # See https://registry.terraform.io/providers/hashicorp/helm/2.5.1
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = ">= 2.5.1"
     }
 
+    # See https://registry.terraform.io/providers/hashicorp/kubernetes/2.10.0
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.7.1"
+      version = ">= 2.10.0"
     }
   }
 }
