5dd04b5e50a92b2d811976662c1d4bd35c72eaf2
Matt Benson <gudnabrsam@gmail.com>
2022-09-27 12:42:09 -0500

    undeclare unused experimental feature which stops TF 1.3.0 from functioning with this module

--- terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  # This enables a function that is part of the optional attributes experiment.
-  # See https://www.terraform.io/language/expressions/type-constraints#experimental-optional-object-type-attributes for more information
-  experiments = [
-    module_variable_optional_attrs
-  ]
-}
