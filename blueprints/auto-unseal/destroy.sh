561f864f7a9a3600903629a52cf5d9a45668932a
Christina Andonov <candonov@amazon.com>
2022-12-12 08:15:05 -0800

    destroying ingress first on clean-up

--- blueprints/auto-unseal/destroy.sh
+++ blueprints/auto-unseal/destroy.sh
@@ -2,6 +2,7 @@
 
 set -xe
 
+terraform destroy -target="kubernetes_ingress_v1.vault" -auto-approve
 terraform destroy -target="module.eks_blueprints_kubernetes_addons" -auto-approve
 terraform destroy -target="module.eks_blueprints" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
